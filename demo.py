887326d1bf88fda9f0027b25a1c58cc36a54ab49
longj18 <maxsoccerssport@hotmail.com>
2022-03-23 13:52:07 -0400

    rough draft of filling in the board

--- demo.py
+++ demo.py
@@ -14,22 +14,53 @@ def print_board(board):
     for row in board:
         print("|".join(row))
 
+player_x = input('Select which box to place your x. ')
+player_y = input('Select which box to place your y. ')
 
-top_left = board[0][0]
-top_middle = board[0][1]
-top_right = board[0][2]
-middle_left = board[1][0]
-middle = board[1][1]
-middle_right = board[1][2]
-bottom_left = board[2][0]
-bottom_middle = board[2][1]
-bottom_middle = board[2][2]
+if player_x == 'top left':
+    board[0][0] = "[X]"
+if player_y == 'top left':
+    board[0][0] = "[Y]"
+if player_x == 'top middle':
+    board[0][1] = "[X]"
+if player_y == 'top middle':
+    board[0][1] = "[Y]"
+if player_x == 'top right':
+    board[0][2] = "[X]"
+if player_y == 'top right':
+    board[0][2] = "[Y]"
+
+if player_x == 'middle left':
+    board[1][0] = "[X]"
+if player_y == 'middle left':
+    board[1][0] = "[Y]"
+if player_x == 'middle':
+    board[1][1] = "[X]"
+if player_y == 'middle':
+    board[1][1] = "[Y]"
+if player_x == 'middle right':
+    board[1][2] = "[X]"
+if player_y == 'middle right':
+    board[1][2] = "[Y]"
+
+if player_x == 'bottom left':
+    board[2][0] = "[X]"
+if player_y == 'bottom left':
+    board[2][0] = "[Y]"
+if player_x == 'bottom middle':
+    board[2][1] = "[X]"
+if player_y == 'bottom middle':
+    board[2][1] = "[Y]"
+if player_x == 'bottom right':
+    board[2][2] = "[X]"
+if player_y == 'bottom right':
+    board[2][2] = "[Y]"
 
 
-print(top_left)
 print(board)
 print('\n')
 print_board(board)
+
 
 
 #TODO create a board
