4116a96dec7ca96a6f14e96c59bf6d6f5344bc8d
longj18 <maxsoccerssport@hotmail.com>
2022-03-23 13:32:53 -0400

    create board and list for game

--- demo.py
+++ demo.py
@@ -4,10 +4,10 @@ for row in range(0, 3):
     board.append([])
     if row < 2:
         for col in range(0, 3):
-            board[row].append('_')
+            board[row].append('[ ]')
     if row == 2:
-        for col in range(0,3):
-            board[row].append(' ')
+        for col in range(0, 3):
+            board[row].append('[ ]')
 
 
 def print_board(board):
@@ -15,9 +15,22 @@ def print_board(board):
         print("|".join(row))
 
 
+top_left = board[0][0]
+top_middle = board[0][1]
+top_right = board[0][2]
+middle_left = board[1][0]
+middle = board[1][1]
+middle_right = board[1][2]
+bottom_left = board[2][0]
+bottom_middle = board[2][1]
+bottom_middle = board[2][2]
+
+
+print(top_left)
 print(board)
 print('\n')
 print_board(board)
+
 
 #TODO create a board
 
