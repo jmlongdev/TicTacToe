dca8787560ec30738763b85c98ecf0740abf49c6
longj18 <maxsoccerssport@hotmail.com>
2022-03-24 11:15:30 -0400

    simplified positions. complete for now

--- demo.py
+++ demo.py
@@ -6,11 +6,6 @@ XO = 'x'
 winner = None
 draw = False
 count = 0
-# Another way to create a tictactoe game board
-# for row in range(0, 3):
-#     TTT_board.append([])
-#     for col in range(0, 3):
-#         TTT_board[row].append("_")
 
 
 def print_board(TTT_board):
@@ -46,55 +41,38 @@ def check_win():
         winner = TTT_board[0][2]
         print(f"{winner} won!")
 
-    # if all([row for row in TTT_board[0]]) and winner is None:
-    #     draw = True
-    #     print("Draw!")
     if winner is None:
         count += 1
-        print(count)
+        # print(count)
         if count >= 9:
             draw = True
             print('Draw!')
     return count
 
+
 def player_choice(player_selection, shape):
     global TTT_board
-    if player_selection == 'top left':
+    if player_selection == '1':
         TTT_board[0][0] = shape.upper()
-    if player_selection == 'top middle':
+    if player_selection == '2':
         TTT_board[0][1] = shape.upper()
-    if player_selection == 'top right':
+    if player_selection == '3':
         TTT_board[0][2] = shape.upper()
 
-    if player_selection == 'middle left':
+    if player_selection == '4':
         TTT_board[1][0] = shape.upper()
-    if player_selection == 'middle':
+    if player_selection == '5':
         TTT_board[1][1] = shape.upper()
-    if player_selection == 'middle right':
+    if player_selection == '6':
         TTT_board[1][2] = shape.upper()
 
-    if player_selection == 'bottom left':
+    if player_selection == '7':
         TTT_board[2][0] = shape.upper()
-    if player_selection == 'bottom middle':
+    if player_selection == '8':
         TTT_board[2][1] = shape.upper()
-    if player_selection == 'bottom right':
+    if player_selection == '9':
         TTT_board[2][2] = shape.upper()
     check_win()
-
-
-# def check_tie():
-#     global draw, game
-#     for list in range(len(TTT_board)):
-#         for innerlist in range(len(TTT_board[list])):
-#            if "_" not in TTT_board[list][innerlist] and winner is False:
-#                 draw = True
-#                 print('Draw!')
-#                 game = False
-#             elif winner:
-#                 game = False
-#             else:
-#                 game = True
-
 
 game = True
 while game:
