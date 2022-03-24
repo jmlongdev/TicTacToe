e28bb1880d87d26d2dd35be5febe33f84b423e22
longj18 <maxsoccerssport@hotmail.com>
2022-03-23 23:24:16 -0400

    Game fully functional. need to create a better way to input X' O's

--- demo.py
+++ demo.py
@@ -1,78 +1,109 @@
-board = []
 
-for row in range(0, 3):
-    board.append([])
-    if row < 2:
-        for col in range(0, 3):
-            board[row].append('[ ]')
-    if row == 2:
-        for col in range(0, 3):
-            board[row].append('[ ]')
+# initialize global variables
+TTT_board = [['_']*3, ['_']*3, ['_']*3]
+# TTT_board = []
+XO = 'x'
+winner = None
+draw = False
+count = 0
+# Another way to create a tictactoe game board
+# for row in range(0, 3):
+#     TTT_board.append([])
+#     for col in range(0, 3):
+#         TTT_board[row].append("_")
 
 
-def print_board(board):
-    for row in board:
+def print_board(TTT_board):
+    for row in TTT_board:
         print("|".join(row))
 
+def check_win():
+    global TTT_board, winner, draw, count
+    # Check for winning rows
+    for row in range(0, 3):
+        if TTT_board[row][0] == TTT_board[row][1] == TTT_board[row][2] and TTT_board[row][0] != "_":
+            # this row won
+            winner = TTT_board[row][0]
+            print(f"{winner} won!")
+            break
+    # Check for winning columns
+    for col in range(0, 3):
+        if TTT_board[0][col] == TTT_board[1][col] == TTT_board[2][col] and TTT_board[0][col] != "_":
+            # this col won
+            winner = True
+            winner = TTT_board[0][col]
+            print(f"{winner} won!")
+            break
 
-player_x = input('Select which box to place your x. ')
-player_y = input('Select which box to place your y. ')
+    # Check for diagonal winners
+    if TTT_board[0][0] == TTT_board[1][1] == TTT_board[2][2] and TTT_board[0][0] != "_":
+        # game won diagonally left to right
+        winner = TTT_board[0][0]
+        print(f"{winner} won!")
 
-if player_x == 'top left':
-    board[0][0] = "[X]"
-if player_y == 'top left':
-    board[0][0] = "[Y]"
-if player_x == 'top middle':
-    board[0][1] = "[X]"
-if player_y == 'top middle':
-    board[0][1] = "[Y]"
-if player_x == 'top right':
-    board[0][2] = "[X]"
-if player_y == 'top right':
-    board[0][2] = "[Y]"
+    if TTT_board[0][2] == TTT_board[1][1] == TTT_board[2][0] and TTT_board[0][2] != "_":
+        # game won diagonally right to left
+        winner = TTT_board[0][2]
+        print(f"{winner} won!")
 
-if player_x == 'middle left':
-    board[1][0] = "[X]"
-if player_y == 'middle left':
-    board[1][0] = "[Y]"
-if player_x == 'middle':
-    board[1][1] = "[X]"
-if player_y == 'middle':
-    board[1][1] = "[Y]"
-if player_x == 'middle right':
-    board[1][2] = "[X]"
-if player_y == 'middle right':
-    board[1][2] = "[Y]"
+    # if all([row for row in TTT_board[0]]) and winner is None:
+    #     draw = True
+    #     print("Draw!")
+    if winner is None:
+        count += 1
+        print(count)
+        if count >= 9:
+            draw = True
+            print('Draw!')
+    return count
 
-if player_x == 'bottom left':
-    board[2][0] = "[X]"
-if player_y == 'bottom left':
-    board[2][0] = "[Y]"
-if player_x == 'bottom middle':
-    board[2][1] = "[X]"
-if player_y == 'bottom middle':
-    board[2][1] = "[Y]"
-if player_x == 'bottom right':
-    board[2][2] = "[X]"
-if player_y == 'bottom right':
-    board[2][2] = "[Y]"
+def player_choice(player_selection, shape):
+    global TTT_board
+    if player_selection == 'top left':
+        TTT_board[0][0] = shape.upper()
+    if player_selection == 'top middle':
+        TTT_board[0][1] = shape.upper()
+    if player_selection == 'top right':
+        TTT_board[0][2] = shape.upper()
 
+    if player_selection == 'middle left':
+        TTT_board[1][0] = shape.upper()
+    if player_selection == 'middle':
+        TTT_board[1][1] = shape.upper()
+    if player_selection == 'middle right':
+        TTT_board[1][2] = shape.upper()
 
-print(board)
-print('\n')
-print_board(board)
-
+    if player_selection == 'bottom left':
+        TTT_board[2][0] = shape.upper()
+    if player_selection == 'bottom middle':
+        TTT_board[2][1] = shape.upper()
+    if player_selection == 'bottom right':
+        TTT_board[2][2] = shape.upper()
+    check_win()
 
 
-#TODO detect a win, based on rows, col, or diag. write a function to check rows, cols, diag
+# def check_tie():
+#     global draw, game
+#     for list in range(len(TTT_board)):
+#         for innerlist in range(len(TTT_board[list])):
+#            if "_" not in TTT_board[list][innerlist] and winner is False:
+#                 draw = True
+#                 print('Draw!')
+#                 game = False
+#             elif winner:
+#                 game = False
+#             else:
+#                 game = True
 
-#TODO crate check_win func
 
-#TODO while loop and check game over or still playing
-
-
-
-
-
+game = True
+while game:
+    player_position = input('Select your position. ')
+    shape = input('Place your shape. ')
+    player_choice(player_position, shape)
+    print_board(TTT_board)
+    # check_tie()
+    if draw or winner:
+        game = False
 
 
--- main.py
+++ main.py
@@ -36,17 +36,14 @@ opening = pg.transform.scale(opening, (width, height + 100))
 # Draw the status bar below the canvas to show which players turn it is and who wins the game
 def draw_status():
     global draw
-
     if winner is None:
         message = XO.upper() + "'s Turn"
     else:
         message = winner.upper() + " won!"
     if draw:
         message = "Game Draw"
-
     font = pg.font.Font(None, 30)
     text = font.render(message, 1, (255, 255, 255))
-
     # copy the rendered message onto the board
     screen.fill((0, 0, 0), (0, 400, 500, 100))
     text_rect = text.get_rect(center=(width/2, 500-50))
@@ -55,25 +52,23 @@ def draw_status():
 
 
 # Draw the grid on the canvas where to play the game
-def game_opening():
-    screen.blit(opening, (0, 0))
-    pg.display.update()
-    time.sleep(1)
-    screen.fill(white)
-
-    # Drawing vertical lines
-    pg.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
-    pg.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
-    # Drawing horizontal lines
-    pg.draw.line(screen, line_color, (0, height/3), (width, height/3), 7)
-    pg.draw.line(screen, line_color, (0, height/3*2), (width, height/3*2), 7)
-    draw_status()
+# def game_opening():
+#     screen.blit(opening, (0, 0))
+#     pg.display.update()
+#     time.sleep(1)
+#     screen.fill(white)
+#     # Drawing vertical lines
+#     pg.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
+#     pg.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
+#     # Drawing horizontal lines
+#     pg.draw.line(screen, line_color, (0, height/3), (width, height/3), 7)
+#     pg.draw.line(screen, line_color, (0, height/3*2), (width, height/3*2), 7)
+#     draw_status()
 
 
 # When someone wins the game or the game is a draw we reset the game
 def check_win():
     global TTT, winner, draw
-
     # Check for winning rows
     for row in range(0, 3):
         if TTT[row][0] == TTT[row][1] == TTT[row][2] and TTT[row][0] is not None:
@@ -81,14 +76,12 @@ def check_win():
             winner = TTT[row][0]
             pg.draw.line(screen, (250, 0, 0), (0, (row+1)*height/3-height/6), (width, (row+1)*height/3-height/6), 4)
             break
-
     # Check for winning columns
     for col in range(0, 3):
         if TTT[0][col] == TTT[1][col] == TTT[2][col] and TTT[0][col] is not None:
             # this col won
             winner = TTT[0][col]
             pg.draw.line(screen, (250, 0, 0), ((col + 1)*width/3 - width/6, 0), ((col + 1)* width/3 - width/6, height), 4)
-
             break
 
     # Check for diagonal winners
@@ -107,64 +100,64 @@ def check_win():
     draw_status()
 
 
-def draw_xo(row, col):
-    global TTT, XO
-    if row == 1:
-        posx = 30
-    if row == 2:
-        posx = width/3+30
-    if row == 3:
-        posx = width/3*2+30
+# def draw_xo(row, col):
+#     global TTT, XO
+#     if row == 1:
+#         posx = 30
+#     if row == 2:
+#         posx = width/3+30
+#     if row == 3:
+#         posx = width/3*2+30
+#
+#     if col == 1:
+#         posy = 30
+#     if col == 2:
+#         posy = width/3+30
+#     if col == 3:
+#         posy = width/3*2+30
+#     TTT[row-1][col-1] = XO
+#     if XO == 'x':
+#         screen.blit(x_img, (posy, posx))
+#         XO = 'o'
+#     else:
+#         screen.blit(o_img, (posy, posx))
+#         XO = 'x'
+#     pg.display.update()
+#     #print(posx, posy)
+#     #print(TTT)
 
-    if col == 1:
-        posy = 30
-    if col == 2:
-        posy = width/3+30
-    if col == 3:
-        posy = width/3*2+30
-    TTT[row-1][col-1] = XO
-    if XO == 'x':
-        screen.blit(x_img, (posy, posx))
-        XO = 'o'
-    else:
-        screen.blit(o_img, (posy, posx))
-        XO = 'x'
-    pg.display.update()
-    #print(posx, posy)
-    #print(TTT)
 
-
-def user_click():
-    # get coordinates of mouse click
-    x, y = pg.mouse.get_pos()
-
-    # get column of mouse click, (1-3)
-    if x < width/3:
-        col = 1
-    elif x < width/3*2:
-        col = 2
-    elif x < width:
-        col = 3
-    else:
-        col = None
-
-    # get row of mouse click (1-3)
-    if y < height/3:
-        row = 1
-    elif y < height/3*2:
-        row = 2
-    elif y < height:
-        row = 3
-    else:
-        row = None
-    # print(row,col)
-
-    if row and col and TTT[row-1][col-1] is None:
-        global XO
-
-        # draw the x or o on the screen
-        draw_xo(row, col)
-        check_win()
+# def user_click():
+#     # get coordinates of mouse click
+#     x, y = pg.mouse.get_pos()
+#
+#     # get column of mouse click, (1-3)
+#     if x < width/3:
+#         col = 1
+#     elif x < width/3*2:
+#         col = 2
+#     elif x < width:
+#         col = 3
+#     else:
+#         col = None
+#
+#     # get row of mouse click (1-3)
+#     if y < height/3:
+#         row = 1
+#     elif y < height/3*2:
+#         row = 2
+#     elif y < height:
+#         row = 3
+#     else:
+#         row = None
+#     # print(row,col)
+#
+#     if row and col and TTT[row-1][col-1] is None:
+#         global XO
+#
+#         # draw the x or o on the screen
+#         draw_xo(row, col)
+#         check_win()
 
 
 # Game reset function
